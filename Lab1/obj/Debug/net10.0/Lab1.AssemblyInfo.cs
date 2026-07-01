53c6ccf228a2708319395fc164040f720ef9696e
andrewhartTRI-C <S01387851@acad.tri-c.edu>
2026-07-01 12:36:31 +0000

    Modified Lab 1

--- Lab1/obj/Debug/net10.0/Lab1.AssemblyInfo.cs
+++ Lab1/obj/Debug/net10.0/Lab1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5a2119dc060462cfc134dd49733e7809a906369")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11f903b703bec6b9acc31c366de90f031a271804")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
