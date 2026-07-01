0be37d04842b84b70c708c15dbf10b77d41246a8
andrewhartTRI-C <S01387851@acad.tri-c.edu>
2026-07-01 12:39:52 -0400

    Update

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -4,8 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        string name = "Andrew";
-        Console.WriteLine("Hello,  " + name + "!");
-        Console.WriteLine("Welcome to IT-1050!");
+        /* Step 1: Andrew Hart
+        IT-1050 - Lab 1 */
     }
+    //Step 2//
+    Console.WriteLine("Andrew Hart");
+    Console.WriteLine("IT-1050");
 }
