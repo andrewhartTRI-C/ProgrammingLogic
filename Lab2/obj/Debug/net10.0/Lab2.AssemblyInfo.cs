adc1e35fb32ab7704937350a6062148acdf471b9
andrewhartTRI-C <S01387851@acad.tri-c.edu>
2026-07-07 05:39:25 +0000

    Merge.

--- Lab2/obj/Debug/net10.0/Lab2.AssemblyInfo.cs
+++ Lab2/obj/Debug/net10.0/Lab2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b5a91103ca727956ff2dc7c1aca6f0322baaab5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d59d67ef165c62170d2b43edc6e8d19f8893552")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
